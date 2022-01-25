7a996c93d96ba852074e52306e4f1cd9ef58e9ae
Eder João Santini <ederjsantini@gmail.com>
2022-01-25 16:58:30 -0400

    Correção do método RELEASE

---
+++ examples/HoldDownKey.ino
@@ -0,0 +1,41 @@
+/*
+Author: Eder João Santini
+Library Download: https://github.com/ederjsantini/AnalogKeyboard
+Example of using the LIB AnalogKeyboard.
+- Hold down key.
+*/
+#include <Arduino.h>
+#include <AnalogKeyboard.h>
+
+AnalogKeyboard keyboard(33);
+
+// ********************************************************************************************************
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< SETUP >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// ********************************************************************************************************
+void setup() {
+  Serial.begin(115200);
+
+}
+
+// ********************************************************************************************************
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< LOOP >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// ********************************************************************************************************
+void loop() {
+
+  if(keyboard.hold(0)){
+    Serial.println("Key 0 - HOLD");
+  }
+  if(keyboard.hold(1)){
+    Serial.println("Key 1 - HOLD");
+  }
+  if(keyboard.hold(2)){
+    Serial.println("Key 2 - HOLD");
+  }
+  if(keyboard.hold(3)){
+    Serial.println("Key 3 - HOLD");
+  }
+  if(keyboard.hold(4)){
+    Serial.println("Key 4 - HOLD");
+  }
+
+}
